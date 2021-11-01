d9bab067005e835c90978f60eaeed61d90e83f0f
go-0116 <Go.116.matsuda@gmail.com>
2021-10-31 19:18:27 -0700

    add_patern_div[1]

--- locator.py
+++ locator.py
@@ -4,6 +4,7 @@ class Recommendation_for_you(object):
     show_more = (By.XPATH,'//*[@id="main-content"]/div/div/div/div/button')
     top_review_xpath = ['//*[@id="main-content"]/div[3]/div/div[4]/div[3]/div[1]/div[2]/div[2]/div[1]',
     '//*[@id="main-content"]/div[3]/div/div[3]/div[3]/div[1]/div[2]/div[2]/div[1]',
-    '//*[@id="main-content"]/div[3]/div/div[2]/div[3]/div[1]/div[2]/div[2]/div[1]']
+    '//*[@id="main-content"]/div[3]/div/div[2]/div[3]/div[1]/div[2]/div[2]/div[1]',
+    '//*[@id="main-content"]/div[3]/div/div[1]/div[3]/div[1]/div[2]/div[2]/div[1]']
 
     
--- main.py
+++ main.py
@@ -15,6 +15,10 @@ import chromedriver_binary
 import requests
 import traceback
 from locator import Recommendation_for_you
+import json
+
+
+hook_url="https://hooks.slack.com/services/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"
 
 chrome_options = Options()
 #chrome_options.add_argument("--headless", )
@@ -27,7 +31,7 @@ def scraping(URL):
     
     urls = []
     names = []
-    i = 11
+    i = 21
     url_path = '//*[@id="main-content"]/div/div[3]/div[2]/div/div[2]/div[' + str(i) + ']/div'
     name_path = '//*[@id="main-content"]/div/div[3]/div[2]/div/div[2]/div[' + str(i) + ']/div/a/h3'     
     try:
@@ -38,7 +42,7 @@ def scraping(URL):
     except TimeoutException:
         print('a')
     try:
-        while i < 810 :#i=11から800個がmax
+        while i < 820 :#i=21から800個がmax
             url_element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH,url_path)))
             urls.append(url_element.find_element_by_tag_name("a").get_attribute("href"))
             print(url_element.find_element_by_tag_name("a").get_attribute("href"))
@@ -79,10 +83,10 @@ def scraping(URL):
         browser.get(a)
         sleep(5)
         for review_xpath in Recommendation_for_you.top_review_xpath:
-            count = 3
+            count = 1
             try:
                 rate_element = browser.find_element_by_xpath(review_xpath+"/div["+str(count)+"]")
-                for b in range(3):
+                for b in range(4):
                     if is_number(rate_element.text):
                         count += 2 
                         count_element = browser.find_element_by_xpath(review_xpath+"/div["+str(count)+"]")
@@ -106,20 +110,24 @@ def scraping(URL):
             review_rates.append(0)
             review_counts.append(0)
             print('nothing')
-
-    df['Revie_rate'] = review_rate
-    df['Review_count'] = review_count
+    
+    percentage_0 = review_rates.count(0) / len(review_rates)
+    if percentage_0 > 0.5:
+        data=json.dumps({
+            "text":"0が50％以上",
+            "icon_emoji": "💥",
+            "username": "webhookbot",
+            "channel": "#fdm-market-research-scraping"
+            })
+        response = requests.post(hook_url, data=data)
+    df['Revie_rate'] = review_rates
+    df['Review_count'] = review_counts
 
     df_rm = df.index[df.NAME.astype(str).str.contains(
     "マクドナルド|モスバーガー|バーガーキング|ウェンディーズ|ロッテリア|フレッシュネスバーガー|ファーストキッチン|ケンタッキー|吉野家|松屋|すき家|なか卯|ガスト|デニーズ|ロイヤルホスト|ローソン|ほっともっと|ココス|スターバックス|幸楽苑|スシロー|ピザハット|ドミノ・ピザ|ピザーラ|ほっかほっか亭|ジョナサン|サブウェイ|いきなりステーキ|丼丸|大漁丼家|魚丼|てんや",na=False
     )]
     df = df.drop(df_rm)
     return df
-
-    
-
-
-
 
 def filedownload(df):
     csv = df.to_csv(index=True)
@@ -163,9 +171,9 @@ selected_name_5 = st.text_input(
 )
 selected_url = {selected_url_1:selected_name_2,selected_url_2:selected_name_2,selected_url_3:selected_name_3,selected_url_4:selected_name_4,selected_url_5:selected_name_5}
 
-hook_url = 'https://hooks.slack.com/services/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'
 
 if st.button('適用'):
+    
     data_0 = {
         'payload': '{"channel": "#fdm-market-research-scraping", "username": "webhookbot", "text": "開始", "icon_emoji": "✅"}'
         }
@@ -177,10 +185,13 @@ if st.button('適用'):
             df1 = pd.DataFrame()
             df1 = scraping(selected_url_1)
         except Exception as f:
-            error_message = traceback.print_exc()
-            data = {
-            'payload': '{"channel": "#fdm-market-research-scraping", "username": "webhookbot", "text": "エラー発生(eroorcode:f)", "icon_emoji": "💥"}'
-            }
+            error_message = traceback.format_exc()
+            data=json.dumps({
+                "text":f"エラー発生{f}"+error_message,
+                "icon_emoji": "💥",
+                "username": "webhookbot",
+                "channel": "#fdm-market-research-scraping"
+                })
             response = requests.post(hook_url, data=data)
         st.write(selected_name_1 + 'のダウンロードはこちら↓') 
         st.markdown(filedownload(df1), unsafe_allow_html=True)
@@ -198,10 +209,13 @@ if st.button('適用'):
             df2 = pd.DataFrame()
             df2 = scraping(selected_url_2)
         except Exception as f:
-            error_message = traceback.print_exc()
-            data = {
-            'payload': '{"channel": "#fdm-market-research-scraping", "username": "webhookbot", "text": "エラー発生(eroorcode:f)", "icon_emoji": "💥"}'
-            }
+            error_message = traceback.format_exc()
+            data=json.dumps({
+                "text":f"エラー発生{f}"+error_message,
+                "icon_emoji": "💥",
+                "username": "webhookbot",
+                "channel": "#fdm-market-research-scraping"
+                })
             response = requests.post(hook_url, data=data)
         st.write(selected_name_2 + 'のダウンロードはこちら↓') 
         st.markdown(filedownload(df2), unsafe_allow_html=True)
@@ -217,10 +231,13 @@ if st.button('適用'):
             df3 = pd.DataFrame()
             df3 = scraping(selected_url_3)
         except Exception as f:
-            error_message = traceback.print_exc()
-            data = {
-            'payload': '{"channel": "#fdm-market-research-scraping", "username": "webhookbot", "text": "エラー発生(eroorcode:f)", "icon_emoji": "💥"}'
-            }
+            error_message = traceback.format_exc()
+            data=json.dumps({
+                "text":f"エラー発生{f}"+error_message,
+                "icon_emoji": "💥",
+                "username": "webhookbot",
+                "channel": "#fdm-market-research-scraping"
+                })
             response = requests.post(hook_url, data=data)
         st.write(selected_name_3 + 'のダウンロードはこちら↓') 
         st.markdown(filedownload(df3, unsafe_allow_html=True))
@@ -236,10 +253,13 @@ if st.button('適用'):
             df4 = pd.DataFrame()
             df4 = scraping(selected_url_4)
         except Exception as f:
-            error_message = traceback.print_exc()
-            data = {
-            'payload': '{"channel": "#fdm-market-research-scraping", "username": "webhookbot", "text": "エラー発生(eroorcode:f)", "icon_emoji": "💥"}'
-            }
+            error_message = traceback.format_exc()
+            data=json.dumps({
+                "text":f"エラー発生{f}"+error_message,
+                "icon_emoji": "💥",
+                "username": "webhookbot",
+                "channel": "#fdm-market-research-scraping"
+                })
             response = requests.post(hook_url, data=data)
         st.write(selected_name_4 + 'のダウンロードはこちら↓') 
         st.markdown(filedownload(df4), unsafe_allow_html=True)
@@ -255,10 +275,13 @@ if st.button('適用'):
             df5 = pd.DataFrame()
             df5 = scraping(selected_url_5)
         except Exception as f:
-            error_message = traceback.print_exc()
-            data = {
-            'payload': '{"channel": "#fdm-market-research-scraping", "username": "webhookbot", "text": "エラー発生(eroorcode:f)", "icon_emoji": "💥"}'
-            }
+            error_message = traceback.format_exc()
+            data=json.dumps({
+                "text":f"エラー発生{f}"+error_message,
+                "icon_emoji": "💥",
+                "username": "webhookbot",
+                "channel": "#fdm-market-research-scraping"
+                })
             response = requests.post(hook_url, data=data)
         st.write(selected_name_5 + 'のダウンロードはこちら↓') 
         st.markdown(filedownload(df5), unsafe_allow_html=True)
